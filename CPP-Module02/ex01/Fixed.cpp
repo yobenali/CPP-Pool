6cd97c8fef5e408a10bb4db02b4ff5bb97576eb6
Youssef Benali <yobenali@student.1337.ma>
2023-01-08 18:24:48 +0100

    test

---
+++ CPP-Module02/ex01/Fixed.cpp
@@ -0,0 +1,53 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	this->number = 0;
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(float const Fixed)
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->number = Fixed * (1 << this->fNumber);
+}
+
+Fixed::Fixed(const int Fixed)
+{
+	this->number = (Fixed << this->fNumber);
+	std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &data)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = data;
+}
+
+Fixed	&Fixed::operator=(Fixed const &obj)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->number = obj.getRawBits();
+	return *this;
+}
+
+float Fixed::toFloat(void) const
+{
+	return (this->number / 256);
+}
+
+int Fixed::toInt( void ) const
+{
+	return (this->number >> this->fNumber);
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+std::ostream& operator<<(std::ostream& out, const Fixed &obj)
+{
+	out << obj.toFloat();
+	return (out);
+}
--- CPP-Module02/ex01/Fixed.hpp
+++ CPP-Module02/ex01/Fixed.hpp
@@ -10,11 +10,17 @@ class Fixed
 		static const int fNumber = 8;
 public:
 	Fixed();
+	Fixed(float const Fixed);
+	Fixed(const int Fixed);
 	Fixed(const Fixed &data);
 	~Fixed();
 	Fixed &operator=(Fixed const &obj);
 	int getRawBits(void) const;
 	void setRawBits(int const raw);
+	float toFloat( void ) const;
+	int toInt( void ) const;
 };
+
+std::ostream& operator<<(std::ostream& out, const Fixed &obj);
 
 #endif
