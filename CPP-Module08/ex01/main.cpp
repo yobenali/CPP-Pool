24a3eca5c786b7c36b11f82446d6ed28f8c55c76
Youssef Benali <yobenali@student.1337.ma>
2023-01-24 02:29:08 +0100

    template done

--- CPP-Module08/ex01/main.cpp
+++ CPP-Module08/ex01/main.cpp
@@ -12,5 +12,37 @@ int main()
     
     std::cout << sp.shortestSpan() << std::endl;
     std::cout << sp.longestSpan() << std::endl;
+
+    Span tt = Span(3);
+
+    try
+    {
+        std::cout << tt.longestSpan() << std::endl;
+        std::cout << tt.shortestSpan() << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    tt = Span(10000);
+    try
+    {
+        for (int i = 0; i < 10000; i++)
+        {
+            tt.addNumber(i + 1);
+        }
+        std::cout << tt.longestSpan() << std::endl;
+        std::cout << tt.shortestSpan() << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    tt = Span(500);
+    std::vector<int> V;
+    for (int i = 0; i < 500; i++)
+        V.push_back(i + 1);
+    tt.addNumber(V.begin(), V.end());
     return 0;
 }
