0cee7958ae18a50f41ddd4070594b03858ec916e
Youssef Benali <yobenali@student.1337.ma>
2023-04-17 02:30:26 +0000

    ....

--- CPP-Module09/ex02/main.cpp
+++ CPP-Module09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yobenali <yobenali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 23:58:02 by yobenali          #+#    #+#             */
-/*   Updated: 2023/04/16 11:31:39 by yobenali         ###   ########.fr       */
+/*   Updated: 2023/04/17 02:29:50 by yobenali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,31 +49,46 @@ void	parsing(int ac, char **av)
 	}
 }
 
-void	inserionSort(std::vector<int> &myVector)
+void	inserionSort(std::vector<int> &myVector, int start, int end)
 {
-	unsigned long	i = 0;
+	int	i = start;
 	int	j;
 	int tmp;
 
-	while (i < myVector.size())
+	while (start <= end)
 	{
-		j = i;
-		while (j > 0 && myVector[j - 1] > myVector[j])
+		j = start;
+		while (j > i && myVector[j - 1] > myVector[j])
 		{
 			tmp = myVector[j - 1];
 			myVector[j - 1] = myVector[j];
 			myVector[j] = tmp;
 			j--;
 		}
-		i++;
+		start++;
 	}
+}
+
+void	mergeInsertSort(std::vector<int> &myVector,  int start, int end)
+{
+	if (end - start <= 10)
+	{
+		inserionSort(myVector, start, end);
+		return;
+	}
+	int mid = (end - start) / 2;
+	mergeInsertSort(myVector, start, mid);
+	mergeInsertSort(myVector, mid + 1, end);
+	// std::cout << mid << std::endl;
+	
 }
 
 int	main(int ac, char **av)
 {
 	int	i = 1;
-	unsigned long p = 0;
+	// unsigned long p = 0;
 	std::vector<int> myVector;
+	
 	if (ac > 1)
 	{
 		parsing(ac, av);
@@ -82,12 +97,14 @@ int	main(int ac, char **av)
 			myVector.push_back(atoi(av[i]));
 			i++;
 		}
-		inserionSort(myVector);
-		while (p < myVector.size())
-		{
-			std::cout << myVector[p] << std::endl;
-			p++;
-		} 
+		// std::cout << myVector.size() << std::endl;
+		mergeInsertSort(myVector, 0, myVector.size() - 1);
+		
+		// while (p < myVector.size())
+		// {
+		// 	std::cout << myVector[p] << std::endl;
+		// 	p++;
+		// } 
 	}
 	else 
 		std::cerr << "Error args" << std::endl;
